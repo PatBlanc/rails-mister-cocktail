c5b76e0bffc3673044f2562d3101172ee0094a09
Patrick Blanc <perso@patrickblanc.net>
2020-03-05 12:25:43 +0100

    models done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+cocktails = Cocktail.create([{name: 'Mojito'}, {name: 'Whiskey Sour'}, {'Mint Julep'}])
+Ingredient.create([{name: 'lemon'}, {name: 'ice'}, {'mint leaves'}])
